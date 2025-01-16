4cabe34d3e365cd0994b6c58007505b4ef07990d
crodpar637 <crodpar637@ieshnosmachado.org>
2025-01-16 09:00:14 +0100

    Auto save changes

--- frontend_bar/src/components/ListadoPlatos.jsx
+++ frontend_bar/src/components/ListadoPlatos.jsx
@@ -6,20 +6,25 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
 
 function ListadoPlatos() {
+    const [rows, setRows] = useState([]);
+
+    useEffect(() => { 
+        async function getPlatos() {
+            let response = await fetch("http://localhost:3000/api/platos");
+            
+            if (response.ok) {
+                let data = await response.json();
+                setRows(data.datos);
+            }
+        }
+
+        getPlatos();
+    },[]); // Se ejecuta solo en el primer renderizado
+
   return (
     <>
       <Typography variant="h4">Listado de platos</Typography>
@@ -28,26 +33,23 @@ function ListadoPlatos() {
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell align="right">Calories</TableCell>
-              <TableCell align="right">Fat&nbsp;(g)</TableCell>
-              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+              <TableCell>IDPLATO</TableCell>
+              <TableCell align="right">NOMBRE</TableCell>
+              <TableCell align="right">DESCRIPCIÓN</TableCell>
+              <TableCell align="right">PRECIO</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {rows.map((row) => (
               <TableRow
-                key={row.name}
+                key={row.idplato}
                 sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
               >
-                <TableCell component="th" scope="row">
-                  {row.name}
-                </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
+                <TableCell align="right">{row.idplato}</TableCell>
+                <TableCell >{row.nombre}</TableCell>
+                <TableCell >{row.descripcion}</TableCell>
+                <TableCell align="right">{row.precio}</TableCell>
+
               </TableRow>
             ))}
           </TableBody>

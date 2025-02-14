b6fb282e94b6515647abffda48c196b45b9f7306
crodpar637 <crodpar637@ieshnosmachado.org>
2025-02-14 13:06:06 +0100

    Auto save changes

--- react-native-NewsApp/App.js
+++ react-native-NewsApp/App.js
@@ -64,7 +64,7 @@ export default function App() {
         <Tab.Screen
           name="Tech"
           component={News}
-          initialParams={{ category: "tech" }}
+          initialParams={{ category: "technology" }}
           options={{
             tabBarIcon: (props) => (
               <Icon
